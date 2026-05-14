4656f26941230c27000c4e0cd95e0a78d4b6cc6f
Felix Wong <56217281+felix1212@users.noreply.github.com>
2026-05-14 14:33:38 +0800

    Add AWS credentials to hello_world.py
    Added AWS credentials to the hello_world.py file.

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,8 @@
 def main() -> None:
     print("Hello, world!")
 
+AWS_ACCESS_KEY_ID=AKIAIOSFODNN7EXAMPLE
+AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY
 
 if __name__ == "__main__":
     main()
